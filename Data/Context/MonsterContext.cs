4579a75eaa752d6a2a765bf6fab6673a2d521104
Ayoub Khloufi <2531003@etu.cchic.ca>
2025-09-04 10:24:44 -0400

    Initial commit

---
+++ Data/Context/MonsterContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyLittleRPG.Models;
+
+namespace MyLittleRPG.Data.Context
+{
+    public class MonsterContext : DbContext
+    {
+        public DbSet<Monster> Monsters { get; set; }
+
+        public MonsterContext(DbContextOptions<MonsterContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Monster>().HasKey(m => m.idMonster);
+        }
+    }
+}
---
+++ Models/Monster.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLittleRPG.Models
+{
+    public class Monster
+    {
+        [Key]
+        public int idMonster { get; set; }
+        public int pokemonId { get; set; }
+        public string nom { get; set; }
+        public int pointsVieBase { get; set; }
+        public int forceBase { get; set; }
+        public int defenseBase { get; set; }
+        public int experienceBase { get; set; }
+        public string spriteURL { get; set; }
+        public string type1 { get; set; }
+        public string type2 { get; set; }
+
+        public Monster() { }
+
+    }
+}
